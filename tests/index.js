88c10c7492aa7a593abf48917f508166fe48e30b
Anthony Fu <anthonyfu117@hotmail.com>
2017-06-21 01:19:07 +0800

    Add travis ci

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,19 @@
+const expect    = require("chai").expect
 const HJDict = require('../dist/hjdict')
 
-let queries = ['傘', '私']
-
-for (let query of queries) {
-  console.log('Start querying ' + query)
-  HJDict.jp2cn(query, data => {
-    console.log(JSON.stringify(data, null, 2))
+describe('basic', ()=> {
+  describe('jp2cn', ()=> {
+    it('reponses correctly', ()=>{
+      const query = '学生'
+      HJDict.jp2cn(query, data => {
+        expect(data.query).to.equal(query)
+        expect(data.from).to.equal('jp')
+        expect(data.to).to.equal('cn')
+        expect(data.explains.length).to.equal(1) // This depends on HJDict.com, may change
+        expect(data.explains[0].primary).to.equal(query)
+        expect(data.explains[0].primary).to.equal('がくせい')
+        expect(data.explains[0].primary).to.equal('gakusei')
+      })
+    })
   })
-}
+})
